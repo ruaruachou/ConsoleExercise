c5ecc0d27fb7493d951c604df4303312dcee8f69
ruaruachou <git config --global user.email>
2023-03-06 11:12:05 +0800

    第一次提交
    描述

---
+++ ConsoleExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
